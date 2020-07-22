ee1a847740ac0695f55bb101788ce33279b49b3d
sameerul97 <sameerul97@gmail.com>
2020-07-22 17:09:12 +0100

    Add singleplayer game words

--- controllers/singleplayer.js
+++ controllers/singleplayer.js
@@ -4,40 +4,49 @@ var router = express.Router();
 // var { drawing } = require("../data/googledrawing_processed_raw_data.json");
 var { drawing } = require("../data/googledrawings.json");
 const WordService = require("../services/wordService");
+const SingleplayerService = require("../services/singleplayerService");
 const {
   Singleplayer_GuestMode,
   Singleplayer_GuestMode_Words,
 } = require("../entity/sequelize");
+const singleplayerService = require("../services/singleplayerService");
 
 router.get("/", function (req, res) {
   res.json({ message: "Guesstheword app singleplayer Api" });
 });
 
 router.get("/word", async function (req, res) {
+  const drawingAndWords = await singleplayerService.drawingWords();
+  for (i in drawingAndWords) {
+    drawingAndWords[i]["word_id"] = i;
+    drawingAndWords[i]["options"] = WordService.getWord(
+      drawingAndWords[i].word
+    ).options;
+    console.log(drawingAndWords[i]);
+  }
+  forDb = drawingAndWords.map((item) => ({
+    word_id: item.word_id,
+    word: item.word,
+    options: item.options,
+  }));
+  forClient = drawingAndWords.filter((item) => delete item.word);
+  // console.log(forClient);
+
   var createdSinglePlayerGame = await Singleplayer_GuestMode.create(
     {
       username: "sameer",
-      words: [
-        {
-          word: "Fire",
-          options: ["Water", "Fire", "Car", "Cloud"],
-        },
-        {
-          word: "Water",
-          options: ["Rain", "Sky", "Train", "Water"],
-        },
-      ],
+      words: forDb,
     },
     {
       include: ["words"],
     }
   );
-  var selectedDrawing =
-    drawing[Math.floor(Math.random() * (drawing.length - 1))];
+
   res.json({
-    drawing: selectedDrawing.drawing,
-    words: WordService.getWord(selectedDrawing.word),
-    game: createdSinglePlayerGame,
+    forClient,
+    // drawing: selectedDrawing.drawing,
+    // words: WordService.getWord(selectedDrawing.word),
+    // game: createdSinglePlayerGame,
   });
 });
 
---
+++ services/singleplayerService.js
@@ -0,0 +1,43 @@
+const WordService = require("./wordService");
+
+var { drawing } = require("../data/googledrawings.json");
+
+module.exports = {
+  drawingWords: async function () {
+    // var selectedDrawing =
+    //   drawing[Math.floor(Math.random() * (drawing.length - 1))];
+    var words = [];
+    while (words.length !== 5) {
+      var selectedDrawing =
+        drawing[Math.floor(Math.random() * (drawing.length - 1))];
+      var { word } = selectedDrawing;
+      var selectedDrawing2 = selectedDrawing.drawing;
+      var found = false;
+      for (var drawings in words) {
+        if (words[drawings].word === selectedDrawing.word) {
+          found = true;
+          break;
+        }
+      }
+      if (!found) {
+        words.push({ word, drawing: selectedDrawing2 });
+      }
+    }
+    // var words = [];
+    // while (words.length !== 5) {
+    //   var givenword = WordService.getWord();
+    //   var { chosenWord } = givenword;
+    //   var found = false;
+    //   for (var word in words) {
+    //     if (words[word].chosenWord === chosenWord) {
+    //       found = true;
+    //       break;
+    //     }
+    //   }
+    //   if (!found) {
+    //     words.push(givenword);
+    //   }
+    // }
+    return words;
+  },
+};
