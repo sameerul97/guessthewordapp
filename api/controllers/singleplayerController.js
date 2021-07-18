a657cdce353c482c7b4c38d704dba3ee71e5f4f6
sameerul97 <sameerul97@gmail.com>
2021-07-18 19:45:08 +0100

    Update docker config Remove console statements

--- api/controllers/singleplayerController.js
+++ api/controllers/singleplayerController.js
@@ -27,35 +27,14 @@ module.exports = {
 
       var drawingAndWords = await SingleplayerService.drawingWords();
 
-      console.log(drawingAndWords);
-
       var drawingAndWords = await SingleplayerService.getWordAndOptions(
         drawingAndWords
       );
-
-      // for (i in drawingAndWords) {
-      //   drawingAndWords[i]["options"] = WordService.getWord(
-      //     drawingAndWords[i].word
-      //   ).options;
-      // }
-
-      console.log();
-      console.log();
-      console.log();
-      console.log(drawingAndWords);
-      console.log();
-      console.log();
-      console.log();
 
       forDb = drawingAndWords.map((item) => ({
         word: item.word,
         options: item.options,
       }));
-
-      // words =
-      //   process.env.NODE_ENV === "production"
-      //     ? drawingAndWords.filter((item) => delete item.word)
-      //     : drawingAndWords;
 
       words = drawingAndWords;
 
@@ -63,14 +42,6 @@ module.exports = {
         username,
         forDb
       );
-      
-      console.log();
-      console.log();
-      console.log();
-      console.log(createdSinglePlayerGame);
-      console.log();
-      console.log();
-      console.log();
 
       var optionsAndWords = createdSinglePlayerGame.words;
 
@@ -116,8 +87,6 @@ module.exports = {
       }
 
       if (!Singleplayer_GuestMode_Record.words[0].alreadyGuessed) {
-        // console.log(Singleplayer_GuestMode_Record);
-
         await SingleplayerService.updateAlreadyGuessed(
           Singleplayer_GuestMode_Record,
           round_id,
@@ -168,127 +137,3 @@ module.exports = {
     res.status(404).json({ message: "Not valid endpoint" });
   },
 };
-
-// router.get("/word", async function (req, res) {
-//   try {
-//     var username = req.query.username;
-
-//     if (username === undefined) {
-//       throw new NoUsernameError();
-//     }
-
-//     const drawingAndWords = await SingleplayerService.drawingWords();
-
-//     for (i in drawingAndWords) {
-//       drawingAndWords[i]["options"] = WordService.getWord(
-//         drawingAndWords[i].word
-//       ).options;
-//     }
-
-//     forDb = drawingAndWords.map((item) => ({
-//       word: item.word,
-//       options: item.options,
-//     }));
-
-//     // words =
-//     //   process.env.NODE_ENV === "production"
-//     //     ? drawingAndWords.filter((item) => delete item.word)
-//     //     : drawingAndWords;
-
-//     words = drawingAndWords;
-
-//     var createdSinglePlayerGame = await SingleplayerService.createGame(
-//       username,
-//       forDb
-//     );
-
-//     var optionsAndWords = createdSinglePlayerGame.words;
-//     for (var i = 0, len = words.length; i < len; i++) {
-//       if (optionsAndWords[i].word === words[i].word) {
-//         words[i]["round_id"] = optionsAndWords[i].round_id;
-//       }
-//     }
-
-//     res.json({
-//       game: {
-//         id: createdSinglePlayerGame.uuid,
-//         words: words,
-//         // test: optionsAndWords,
-//       },
-//     });
-//   } catch (error) {
-//     if (error instanceof NoUsernameError) {
-//       res.json({
-//         game: { success: false, message: error.message },
-//       });
-//     }
-//   }
-// });
-
-// router.post("/word", async function (req, res) {
-//   try {
-//     const { game_id, round_id, selected_answer, options } = req.body;
-//     if (round_id != parseInt(round_id)) {
-//       throw new InvalidGameDataError();
-//     }
-
-//     const Singleplayer_GuestMode_Record = await SingleplayerService.verifyAnswer(
-//       game_id,
-//       round_id,
-//       selected_answer,
-//       options
-//     );
-
-//     if (Singleplayer_GuestMode_Record === null) {
-//       throw new InvalidGameDataError();
-//     }
-
-//     if (!Singleplayer_GuestMode_Record.words[0].alreadyGuessed) {
-//       // console.log(Singleplayer_GuestMode_Record);
-
-//       await SingleplayerService.updateAlreadyGuessed(
-//         Singleplayer_GuestMode_Record,
-//         round_id,
-//         game_id
-//       );
-
-//       if (Singleplayer_GuestMode_Record.words[0].word === selected_answer) {
-//         var game = {
-//           success: true,
-//           message: "Valid answer",
-//           data: await SingleplayerService.updateScore(
-//             Singleplayer_GuestMode_Record,
-//             game_id
-//           ),
-//         };
-//       } else {
-//         game = {
-//           success: false,
-//           message: "Invalid answer",
-//           data: Singleplayer_GuestMode_Record,
-//         };
-//       }
-//     } else {
-//       game = {
-//         success: false,
-//         message: "Already Guessed",
-//         data: Singleplayer_GuestMode_Record,
-//       };
-//     }
-
-//     res.json({ game });
-//   } catch (error) {
-//     if (error instanceof ValidationError) {
-//       res.json({
-//         game: { success: false, message: error },
-//       });
-//     }
-//     if (error instanceof InvalidGameDataError) {
-//       res.json({
-//         game: { success: false, message: error.message },
-//       });
-//     }
-//   }
-// });
-
-// module.exports = router;
--- api/model/sequelize.js
+++ api/model/sequelize.js
@@ -2,10 +2,11 @@ const Sequelize = require("sequelize");
 const GameRoomLinkModel = require("../../entity/gameRoomLink");
 const Singleplayer_GuestMode_Model = require("./singleplayer_guestmode");
 const Singleplayer_GuestMode_Words_Model = require("./singleplayer_guestMode_words");
+
 const sequelize = new Sequelize(
-  process.env.PG_DB_NAME || "guessthewordapp_db",
-  process.env.PG_DB_USERNAME || "guessthewordappPgAdmin",
-  process.env.PG_DB_PASSWORD || "guessthewordappPgAdminPassword",
+  process.env.PG_DB_NAME || "guesstheword_db",
+  process.env.PG_DB_USERNAME || "guesstheword_user",
+  process.env.PG_DB_PASSWORD || "guesstheword_password",
   {
     host: process.env.PG_HOST || "db",
     dialect: "postgres",
@@ -18,7 +19,7 @@ const sequelize = new Sequelize(
     await sequelize.authenticate();
     console.log("Connection has been established successfully.");
   } catch (error) {
-    // console.error("Unable to connect to the database:", error);
+    console.error("Unable to connect to the database:", error);
   }
 })();
 
